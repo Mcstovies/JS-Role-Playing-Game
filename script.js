cbbf15a135884a72967853882fad4069ae20e18c
Adam Clarke <adamclarke0905@gmail.com>
2024-11-13 23:06:31 +0000

    created locations objects

--- script.js
+++ script.js
@@ -17,7 +17,12 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
-const locations = [];
+const locations = [
+  {
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"]
+  }
+];
 
 // Initialise buttons
 button1.onclick = goStore;
