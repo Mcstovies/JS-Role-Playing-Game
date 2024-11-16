2d9c26e3ef5209c1cee21f3fc3f60cf07cf7a2d2
Adam Clarke <adamclarke0905@gmail.com>
2024-11-16 15:21:00 +0000

    goCave function to call third locations array

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const locations = [
     "button text": ["Buy 10 health (10 gold)","Buy weapon (30 gold)", "Go to town square"],
     "button functions": [buyHealth, buyWeapon, goTown],
     text: "You enter the store."
-  }
+  },
   {
     name: "cave",
     "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
@@ -62,7 +62,7 @@ update(locations[1]);
 }
 
 function goCave() {
-    console.log("Going to cave.");
+    update(locations[2]);
 }
 
 function fightDragon() {
