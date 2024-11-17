aa801ff30ae7fe4619dd62d9ffa0e7a9bf3d93f6
Adam Clarke <adamclarke0905@gmail.com>
2024-11-17 19:14:48 +0000

    buyWeapon function  updated to tell player what weapon they have, if they have enough gold to buy and checks if they have the best

--- script.js
+++ script.js
@@ -100,6 +100,7 @@ function buyHealth() {
 }
 
 function buyWeapon() {
+  if (currentWeaponIndex < 3){
 if (gold >= 30){
   gold -= 30;
   currentWeaponIndex++;
@@ -107,6 +108,10 @@ if (gold >= 30){
   let newWeapon = weapons[currentWeaponIndex].name;
   text.innerText = "You now have a " + newWeapon + ".";
   inventory.push(newWeapon);
+  text.innerText += " In your inventory you have: " + inventory;
+ } else {
+  text.innerText = "You do not have enough gold to buy a weapon."
+ }
  }
 }
 
