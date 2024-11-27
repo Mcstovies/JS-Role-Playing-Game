85153e3f7405743ce56ca100af625c73b73904e9
Adam Clarke <adamclarke0905@gmail.com>
2024-11-27 19:26:00 +0000

    created monsters array with their respective stats

--- script.js
+++ script.js
@@ -36,6 +36,12 @@ const weapons = [
   }
 ];
 
+const monsters = [
+  {name: "slime", level: 2, health: 15},
+  {name: "fanged beast", level: 8, health: 60},
+  {name: "dragon", level: 20, health: 300}
+]
+
 const locations = [
   {
     name: "town square",
@@ -84,9 +90,7 @@ function goCave() {
     update(locations[2]);
 }
 
-function fightDragon() {
-    console.log("Fighting dragon.")
-}
+
 
 function buyHealth() {
   if (gold >= 10){
@@ -137,4 +141,8 @@ function fightSlime() {
 
 function fightBeast() {
 
+}
+
+function fightDragon() {
+    console.log("Fighting dragon.")
 }
